f823dd61ba8df1e7cb4c243244a5cf19acabc506
lazrius <lazdisco@gmail.com>
2020-04-11 13:24:00 +0100

    Executing assembly would always be the util.dll

--- AssemblyUtils.cs
+++ AssemblyUtils.cs
@@ -6,21 +6,10 @@
 
     public class AssemblyUtils
     {
-        public static string Name(bool executingAssembly) => executingAssembly ? Assembly.GetExecutingAssembly().FullName : Assembly.GetCallingAssembly().FullName;
-        public static Version Version(bool executingAssembly) => executingAssembly ? Assembly.GetExecutingAssembly().GetName().Version : Assembly.GetCallingAssembly().GetName().Version;
-        public static string Company(bool executingAssembly) =>
-            executingAssembly
-                ? FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).CompanyName
-                : FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).CompanyName;
-
-        public static string Description(bool executingAssembly) =>
-            executingAssembly
-                ? FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileDescription
-                : FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).FileDescription;
-
-        public static string Copyright(bool executingAssembly) =>
-            executingAssembly
-                ? FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).LegalCopyright
-                : FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).LegalCopyright;
+        public static string Name => Assembly.GetCallingAssembly().GetName().Name;
+        public static Version Version => Assembly.GetCallingAssembly().GetName().Version;
+        public static string Company => FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).CompanyName;
+        public static string Description => FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).FileDescription;
+        public static string Copyright => FileVersionInfo.GetVersionInfo(Assembly.GetCallingAssembly().Location).LegalCopyright;
     }
 }
